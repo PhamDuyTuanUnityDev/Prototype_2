aa8d2827168e5837e7f35df1c40885eeac969204
PhamDuyTuanUnityDev <109053805+PhamDuyTuanUnityDev@users.noreply.github.com>
2022-07-11 14:31:38 +0700

    Initial commit
    Challenge 2 - Play Fetch

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float delay = 0f;
+    public float timeRate = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        delay += Time.deltaTime;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && delay >= timeRate)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            delay = 0f;
+        }
+    }
+}
