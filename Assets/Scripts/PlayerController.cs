4f5711190d97cecbfc9baaaf320c9e6a8bc391e6
PhamDuyTuanUnityDev <109053805+PhamDuyTuanUnityDev@users.noreply.github.com>
2022-07-11 13:16:12 +0700

    Initial commit
    6.Clean up your code and variables

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,13 +6,18 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10f;
+    public float xRange = 10f;
     private void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
-        if(transform.position.x < -10)
+        if(transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
     }
 
